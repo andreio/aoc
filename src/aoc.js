ca01a67698523073027cc5a4248dc5a2418c5f96
Andrei Oprea <andrewoprea2003@yahoo.com>
2013-11-25 21:13:46 +0100

    - reimplemented library v0.3

--- src/aoc.js
+++ src/aoc.js
@@ -1,5 +1,5 @@
 /**
- * @Author: Oprea Andrei
+ * @Author: Andrei Oprea Constantin
  * @Date: November 2013
  */
 
@@ -7,227 +7,304 @@
 
     'use strict';
 
-    function Config(src) {
-        repair(this);
-        src && this.extend(repair(src));
-    }
-
-    Config.prototype.extend = function (src) {
-        this.actions.default = this.actions.default.concat(src.actions.default);
-        angular.extend(this.actions.custom, src.actions.custom);
-        angular.extend(this.options, src.options);
-        return this;
-    };
-
-    var repair = function (config) {
-        var baseConfig = model();
-        config.actions = config.actions || baseConfig.actions;
-        config.actions.default = config.actions.default || baseConfig.actions.default;
-        config.actions.custom = config.actions.custom || baseConfig.actions.custom;
-        config.options = config.options || baseConfig.options;
-        return config;
-    };
-
-    Config.base = function () {
-        return new Config(model());
-    };
-
-    Config.merge = function (dest, src) {
-        return Config.base().extend(dest).extend(src);
-    };
-
-    var model = function () {
-        return {
-            actions: {
-                default: [],
-                custom: {}
-            },
-            options: {
-
-            }
-        }
-    };
-
     var aoc = angular.module('aoc', [], function () {
     });
 
-    var GENERAL_CONFIGS = {};
-    var SPECIFIC_CONFIGS = {};
+// ********** CONFIG SECTION ************
+    var configs = {};
 
-    function configureGeneral(component, config) {
-        GENERAL_CONFIGS[component] = config;
-    }
-
-    function configureSpecific(component, config, selector) {
-        SPECIFIC_CONFIGS[component] = SPECIFIC_CONFIGS[component] || [];
-        SPECIFIC_CONFIGS[component].push({selector: selector, config: config});
-    }
-
-    function configure(component, config, selector) {
-        if (selector)
-            configureSpecific(component, config, selector);
-        else
-            configureGeneral(component, config);
-    }
-
-    function extractOptions(scope, attrs) {
-        var optionsPattern = /^option[A-Z][A-Za-z]*$/,
-            functionPattern = /^([^(])+\(.*\).*?$/,
-            optionPattern = /^option/,
-            options = {},
-            attr, rAttr;
-
-        function name(pattern, attr) {
-            var rName = attr.replace(pattern, "");
-            return rName[0].toLowerCase() + rName.substr(1);
+    function Config(general) {
+        this.general = general || {};
+        this.specific = [];
+        this.setGeneral = function (config) {
+            this.general = config;
+        };
+        this.addSpecific = function (config, id) {
+            this.specific.push({config: config, id: id});
+        };
+        this.filterSpecific = function (filterFn) {
+            var configs = [];
+            angular.forEach(this.specific, function (e) {
+                if (filterFn(e))
+                    configs.push(e.config);
+            });
+            return configs;
+        };
+        this.aggregate = function (reduceFn, baseConfig, filterFn) {
+            return [this.general].concat(this.filterSpecific(filterFn || function () {
+                return true;
+            })).reduce(reduceFn, baseConfig);
         }
+    }
 
-        for (attr in attrs) {
-            if (!attrs.hasOwnProperty(attr) || !optionsPattern.test(attr))continue;
-            rAttr = name(optionPattern, attr);
-            if (functionPattern.test(attrs[attr]))
-                options[rAttr] =
-                    (function (atr) {
-                        return function () {
-                            var args = Array.prototype.slice.call(arguments),
-                                arg, argNr = 0, ret;
-                            for (arg in args)
-                                scope["$arg" + argNr++] = args[arg];
-                            ret = scope.$eval(attrs[atr]);
-                            while (argNr > -1) {
-                                delete scope["$arg" + argNr];
-                                argNr -= 1;
-                            }
-                            return ret;
-                        }
-                    })(attr);
-            else options[rAttr] = scope.$eval(attrs[attr]);
+    function AocConfig(scope, controlName, element, $injector) {
+        this.element = element;
+        this.controlName = controlName;
+        this.scope = scope;
+        this.evaluate = function () {
+            var config = evaluateConfig(configs[controlName] || new Config(function () {
+                return {};
+            }), element, function (config) {
+                return $injector.invoke(config, {}, {'scope': scope});
+            });
+            config.element = element;
+            config.controlName = controlName;
+            config.scope = scope;
+            return config;
+        };
 
+    }
+
+
+    function configure(controlName, configuration, selector) {
+        var config = (configs[controlName] || (configs[controlName] = new Config()));
+        selector ? config.addSpecific(configuration, selector) : config.setGeneral(configuration);
+    }
+
+    aoc.configure = configure;
+
+    function evaluateConfig(config, element, injectFn) {
+        return config.aggregate(
+            function (prevConfig, thisConfig) {
+                thisConfig = injectFn(thisConfig);
+                var retConfig = {};
+                angular.extend(retConfig, prevConfig);
+                (thisConfig.actions) && (retConfig.actions.default = retConfig.actions.default.concat(thisConfig.actions.default || []));
+                (thisConfig.actions) && (angular.extend(retConfig.actions.custom, thisConfig.actions.custom || {}));
+                (thisConfig.actions) && (retConfig.actions.defaultInvoker = (thisConfig.actions.defaultInvoker || retConfig.actions.defaultInvoker()));
+                (thisConfig.options) && (angular.extend(retConfig.options, thisConfig.options));
+                (thisConfig.events) && (angular.extend(retConfig.events, thisConfig.events));
+                (thisConfig.createFn) && (retConfig.createFn = thisConfig.createFn);
+                return retConfig;
+            }, {
+                actions: {
+                    default: [],
+                    custom: {},
+                    defaultInvoker: function (element, controlName, args) {
+                        controlName[controlName].call(element, args);
+                    }
+                },
+                options: {},
+                events: {},
+                createFn: function (element, controlName, options) {
+                    return element[controlName](options);
+                }
+            }, function (selector) {
+                return element.is(selector);
+            }
+        )
+    }
+
+//************** CONTROL SECTION ***********************
+
+    function proxyInScope(scope, fn) {
+        if (!scope.$$phase) {
+            scope.$apply(function () {
+                fn();
+            });
+        } else {
+            fn();
         }
-        return options;
     }
 
+    function aocBind(scope, element, event, fn, context) {
+        element.bind(event,
+            function () {
+                var args = Array.prototype.slice.call(arguments);
+                proxyInScope(scope, function () {
+                    fn.apply(context || element, args);
+                });
 
-    function registerComponent(component, id, container, controller) {
-        if (!container.hasOwnProperty('components'))
-            container.components = {};
-        if (!container.components.hasOwnProperty(component))
-            container.components[component] = {};
-        container.components[component][id] = controller;
+            });
     }
 
-    function extractConfig(element, component, scope, controller) {
-        var gConfig = GENERAL_CONFIGS[component] || function () {
-                return {}
-            },
-            sConfig = SPECIFIC_CONFIGS[component] || [],
-            generalConfigs = new Config(gConfig.call(Function.caller, scope, controller)),
-            specificConfigs = (sConfig.map(function (c) {
-                if (element.is(c.selector))
-                    return new Config(c.config.call(Function.caller, scope, controller));
-                return new Config();
-            }));
-        return [generalConfigs].concat(specificConfigs).reduce(Config.merge, Config.base());
-    }
+    function Control(config) {
+        (!config.element[0].aoc) && (config.element[0].aoc = {});
+        (!config.element[0].aoc.controls) && (config.element[0].aoc.controls = {});
+        config.element[0].aoc.controls[config.controlName] = this;
 
-    function Controller(component, element, ioc) {
+        var control = this;
+        this._getConfig = function () {
+            return config;
+        };
 
-        this._getComponent = function () {
-            return component;
+        this._getControlName = function () {
+            return config.controlName;
         };
         this._getElement = function () {
-            return element
+            return config.element;
         };
-        this._getWidget = function () {
-            return element[component]('widget');
+        this._bind = function (event, fn) {
+            aocBind(config.scope, config.element, event, fn, control);
         };
-        this._setWidgetOption = function (option, value) {
-            return this._getElement()[this._getComponent()]('option', option, value);
+        this._trigger = function (event) {
+            config.element.trigger(event);
         };
-        var widgetCreatedCallbacks = [];
-        this._onWidgetCreated = function (fn) {
-            widgetCreatedCallbacks.push(fn);
+
+        this._addDefaultAction = function (action, defaultInvoker) {
+            this[action] = function () {
+                var args = [action].concat(Array.prototype.slice.call(arguments));
+                proxyInScope(config.scope, function () {
+                    defaultInvoker(config.element, config.controlName, args);
+                });
+            };
         };
-        var _this = this;
-        ioc.triggerWidgetCreated = function () {
-            for (var fn in widgetCreatedCallbacks) {
-                if (!widgetCreatedCallbacks.hasOwnProperty(fn))continue;
-                widgetCreatedCallbacks[fn].call(_this, _this);
+        this._addCustomAction = function (action, fn) {
+            this[action] = function () {
+                var args = [action].concat(Array.prototype.slice.call(arguments));
+                proxyInScope(config.scope, function () {
+                    fn.apply(control, args);
+                });
             }
         };
-
     }
 
-    Controller.calibrate = function (instance, scope, config) {
-        var action,
-            proxyInScope = function (fn) {
-                if (!scope.$$phase) {
-                    scope.$apply(function () {
-                        fn();
-                    });
-                } else {
-                    fn();
-                }
-            }, actionArgs = function (action, args) {
-                return [action].concat(Array.prototype.slice.call(args));
-            },
-            setDefaultAction = function (action) {
-                instance[action] = function () {
-                    var args = actionArgs(action, arguments);
-                    proxyInScope(function () {
-                        config.factory.defaultActionsInvoker(args);
-                    });
-                }
-            },
-            setCustomAction = function (action) {
-                instance[action] = function () {
-                    var args = actionArgs(action, arguments);
-                    proxyInScope(function () {
-                        config.actions.custom[action].apply(instance, args);
-                    });
-                }
-            };
+    Control.initControl = function (instance) {
+        var config = instance._getConfig().evaluate();
 
-        for (action in config.actions.default) {
-            if (!config.actions.default.hasOwnProperty(action))continue;
-            setDefaultAction(config.actions.default[action]);
-        }
-        for (action in config.actions.custom) {
-            if (!config.actions.custom.hasOwnProperty(action))continue;
-            setCustomAction(action);
-        }
+        angular.forEach(config.actions.default, function (action) {
+            instance._addDefaultAction(action);
+        });
+        angular.forEach(config.actions.custom, function (fn, action) {
+            instance._addCustomAction(action, fn);
+        });
+        angular.forEach(config.events, function (fn, event) {
+            instance._bind(event, fn);
+        });
 
+        var options = config.options;
+        (config.element[0].aoc.options) && (angular.extend(options, config.element[0].aoc.options[config.controlName] || {}));///options directive used
+
+        var widget = config.createFn.call(instance, config.element, config.controlName, options);
+        instance._trigger('controlCreated', {widget: widget, control: instance});
+        return widget;
     };
 
-    aoc.directive('aoc', function ($compile) {
-        return {
 
-            scope: true,
-            link: function (scope, element, attr) {
-                var component = attr.component,
-                    id = attr.id,
-                    trigger = {},
-                    controller = new Controller(component, element, trigger),
-                    config = extractConfig(element, component, scope, controller),
-                    options = angular.extend(config.options, extractOptions(scope, attr)),
-                    jQueryWidgetFactory = {
-                        create: function () {
-                            return element[component](options);
-                        },
-                        defaultActionsInvoker: function (args) {
-                            element[component].apply(element, args);
-                        }
-                    };
-                config.factory = config.factory || jQueryWidgetFactory;
+    //**************** EVALUATOR *********************
 
-                config.factory.create();
-                Controller.calibrate(controller, scope, config);
-                registerComponent(component, id, scope.$parent, controller);
-                trigger.triggerWidgetCreated();
+    function Evaluator(scope) {
+        this.eFunction = function (expression, locals) {
+            return function () {
+                var args = Array.prototype.slice.call(arguments);
+                proxyInScope(scope, function () {
+                    var ret, argsVar = "$args";
+                    scope[argsVar] = args;
+                    angular.forEach(locals, function (local, name) {
+                        scope["$" + name] = local;
+                    });
+                    ret = scope.$eval(expression);
+                    delete scope[argsVar];
+                    angular.forEach(locals, function (local, name) {
+                        delete scope["$" + name];
+                    });
+                    return ret;
+                });
+            }
+        };
+        this.eValue = function (expression) {
+            return scope.$eval(expression);
+        };
+    }
+
+// ************** AOC DIRECTIVE SECTION ***************** //
+
+    aoc.directive('aoc', function ($compile, $injector, $timeout) {
+        return{
+            priority: 500,
+            require: ['?ngModel' ],
+            link: function (scope, element, attrs) {
+                console.log('aoc');
+                var controls = attrs.aoc.split(' '),
+                    control;
+                console.log('aoc');
+                angular.forEach(controls, function (controlName) {
+                    control = new Control(new AocConfig(scope, controlName, element, $injector));
+                    $timeout(function () {
+                        console.log('timeout');
+                        Control.initControl(control);
+                    });
+
+                });
             }
         }
     });
 
-    aoc.configure = configure;
+// **************** SCOPE DIRECTIVE SECTION **************************
 
-})(angular);
+    aoc.directive('scope', function () {
+        return{
+            priority: 100,
+            scope: true,
+            link: function () {
+                console.log('scope');
+            }
+        }
+    });
+// **************** EVENTS DIRECTIVE SECTION *************************
+
+    aoc.directive('events', function () {
+        return{
+            priority: 999,
+            link: function (scope, element, attr) {
+                var eventPrefix = attr.events,
+                    eventsPattern = new RegExp('^' + eventPrefix + '[A-Z][A-Za-z]*$'),
+                    eventPattern = new RegExp('^' + eventPrefix),
+                    evaluator = new Evaluator(scope), eventFn, eventName;
+                (!element[0].aoc) && (element[0].aoc = {});
+                angular.forEach(attr, function (expr, event) {
+                    if (!eventsPattern.test(event)) return;
+                    eventFn = evaluator.eFunction(expr, {controls: element[0].aoc.controls});
+                    eventName = event.replace(eventPattern, "");
+                    eventName = eventName[0].toLowerCase() + eventName.substr(1);
+                    aocBind(scope, element, eventName, eventFn);
+
+                })
+            }
+        }
+    });
+
+// ************** OPTIONS DIRECTIVE SECTION ********************************
+    aoc.directive('options', function () {
+        return {
+            priority: 999,
+            link: function (scope, element, attr) {
+                console.log('options');
+                var evaluator = new Evaluator(scope),
+                    prefixes = evaluator.eValue(attr.options),
+                    optionsPrefix, optionPrefix, optionName, optionValue, control;
+                (!element[0].aoc) && (element[0].aoc = {});
+                (!element[0].aoc.options) && (element[0].aoc.options = {});
+                angular.forEach(prefixes, function (prefix, comp) {
+                    optionsPrefix = new RegExp('^' + prefix + '[A-Z][A-Za-z]*$');
+                    optionPrefix = new RegExp('^' + prefix);
+                    (element[0].aoc.controls) && (control = element[0].aoc.controls[prefix]);
+                    element[0].aoc.options[comp] = {};
+                    angular.forEach(attr, function (expr, option) {
+                        if (!optionsPrefix.test(option))return;
+                        optionName = option.replace(optionPrefix, "");
+                        optionName = optionName[0].toLowerCase() + optionName.substr(1);
+                        switch (expr[0]) {
+                            case '*':
+                                optionValue = '{{' + expr.substr(1) + "}}";
+                                break;
+                            case '+':
+                                optionValue = evaluator.eFunction(expr.substr(1), control);
+                                break;
+                            case '=':
+                                optionValue = evaluator.eValue(expr.substr(1));
+                                break;
+                            default:
+                                optionValue = expr;
+                                break;
+                        }
+                        element[0].aoc.options[comp][optionName] = optionValue;
+                    });
+
+                })
+
+            }
+        }
+    })
+})
+    (angular);
