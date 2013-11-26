cf793046c2873c24076ed724941ad7073ceeb4d2
Andrei Oprea <andrewoprea2003@yahoo.com>
2013-11-26 01:21:17 +0100

    Fixed some bugs. Improved evaluation mechanisms

--- src/aoc.js
+++ src/aoc.js
@@ -14,7 +14,7 @@
     var configs = {};
 
     function Config(general) {
-        this.general = general || {};
+        this.general = general;
         this.specific = [];
         this.setGeneral = function (config) {
             this.general = config;
@@ -25,7 +25,7 @@
         this.filterSpecific = function (filterFn) {
             var configs = [];
             angular.forEach(this.specific, function (e) {
-                if (filterFn(e))
+                if (filterFn(e.id))
                     configs.push(e.config);
             });
             return configs;
@@ -37,41 +37,37 @@
         }
     }
 
-    function AocConfig(scope, controlName, element, $injector) {
+    function AocConfig(scope, controlName, element, invokeFunction) {
+        var config = evaluateConfig(configs[controlName] || new Config(function () {
+            return {};
+        }), element, function (config) {
+            return invokeFunction.call(this, config);
+        });
+        angular.extend(this, config);
         this.element = element;
         this.controlName = controlName;
         this.scope = scope;
-        this.evaluate = function () {
-            var config = evaluateConfig(configs[controlName] || new Config(function () {
-                return {};
-            }), element, function (config) {
-                return $injector.invoke(config, {}, {'scope': scope});
-            });
-            config.element = element;
-            config.controlName = controlName;
-            config.scope = scope;
-            return config;
-        };
-
     }
 
 
     function configure(controlName, configuration, selector) {
-        var config = (configs[controlName] || (configs[controlName] = new Config()));
+        var config = (configs[controlName] || (configs[controlName] = new Config(function () {
+            return {}
+        })));
         selector ? config.addSpecific(configuration, selector) : config.setGeneral(configuration);
     }
 
     aoc.configure = configure;
 
-    function evaluateConfig(config, element, injectFn) {
+    function evaluateConfig(config, element, invokeFunction) {
         return config.aggregate(
             function (prevConfig, thisConfig) {
-                thisConfig = injectFn(thisConfig);
+                thisConfig = invokeFunction(thisConfig);
                 var retConfig = {};
                 angular.extend(retConfig, prevConfig);
                 (thisConfig.actions) && (retConfig.actions.default = retConfig.actions.default.concat(thisConfig.actions.default || []));
                 (thisConfig.actions) && (angular.extend(retConfig.actions.custom, thisConfig.actions.custom || {}));
-                (thisConfig.actions) && (retConfig.actions.defaultInvoker = (thisConfig.actions.defaultInvoker || retConfig.actions.defaultInvoker()));
+                (thisConfig.actions) && (retConfig.actions.defaultInvoker = (thisConfig.actions.defaultInvoker || retConfig.actions.defaultInvoker));
                 (thisConfig.options) && (angular.extend(retConfig.options, thisConfig.options));
                 (thisConfig.events) && (angular.extend(retConfig.events, thisConfig.events));
                 (thisConfig.createFn) && (retConfig.createFn = thisConfig.createFn);
@@ -81,12 +77,21 @@
                     default: [],
                     custom: {},
                     defaultInvoker: function (element, controlName, args) {
-                        controlName[controlName].call(element, args);
+                        if (!element[controlName]) {
+                            console.error('defaultInvoker: invalid control name: ' + controlName + ". If this is not a jQuery Widget style component in the form of 'element['controlName'](options)' with the method accessing format'element[control](methodName,arguments)' consider setting a custom defaultInvoker function and createFn function respectively in the configuration of the control with the same control name ");
+                            return
+                        }
+                        ;
+                        element[controlName].apply(element, args);
                     }
                 },
                 options: {},
                 events: {},
                 createFn: function (element, controlName, options) {
+                    if (!element[controlName]) {
+                        console.error('defaultInvoker: invalid control name: ' + controlName + ". If this is not a jQuery Widget style component in the form of 'element['controlName'](options)' with the method accessing format'element[control](methodName,arguments)' consider setting a custom defaultInvoker function and createFn function respectively in the configuration of the control with the same control name ");
+                        return;
+                    }
                     return element[controlName](options);
                 }
             }, function (selector) {
@@ -141,40 +146,38 @@
             config.element.trigger(event);
         };
 
-        this._addDefaultAction = function (action, defaultInvoker) {
+        this._addDefaultAction = function (action) {
             this[action] = function () {
                 var args = [action].concat(Array.prototype.slice.call(arguments));
                 proxyInScope(config.scope, function () {
-                    defaultInvoker(config.element, config.controlName, args);
+                    config.actions.defaultInvoker.call(control, config.element, config.controlName, args);
                 });
             };
         };
         this._addCustomAction = function (action, fn) {
             this[action] = function () {
-                var args = [action].concat(Array.prototype.slice.call(arguments));
+                var args = Array.prototype.slice.call(arguments);
                 proxyInScope(config.scope, function () {
                     fn.apply(control, args);
                 });
             }
         };
+
+        angular.forEach(config.actions.default, function (action) {
+            control._addDefaultAction(action);
+        });
+        angular.forEach(config.actions.custom, function (fn, action) {
+            control._addCustomAction(action, fn);
+        });
+        angular.forEach(config.events, function (fn, event) {
+            control._bind(event, fn);
+        });
     }
 
     Control.initControl = function (instance) {
-        var config = instance._getConfig().evaluate();
-
-        angular.forEach(config.actions.default, function (action) {
-            instance._addDefaultAction(action);
-        });
-        angular.forEach(config.actions.custom, function (fn, action) {
-            instance._addCustomAction(action, fn);
-        });
-        angular.forEach(config.events, function (fn, event) {
-            instance._bind(event, fn);
-        });
-
-        var options = config.options;
+        var config = instance._getConfig(),
+            options = config.options;
         (config.element[0].aoc.options) && (angular.extend(options, config.element[0].aoc.options[config.controlName] || {}));///options directive used
-
         var widget = config.createFn.call(instance, config.element, config.controlName, options);
         instance._trigger('controlCreated', {widget: widget, control: instance});
         return widget;
@@ -214,18 +217,27 @@
             priority: 500,
             require: ['?ngModel' ],
             link: function (scope, element, attrs) {
-                console.log('aoc');
                 var controls = attrs.aoc.split(' '),
-                    control;
-                console.log('aoc');
+                    control, externalScope = attrs.$attr.scope ? scope.$parent : scope;
+                (!externalScope.$aoc) && (externalScope.$aoc = function (selector) {
+                    var element = angular.element(selector);
+                    if (!(element.length && element[0].aoc.controls))return;
+                    return element[0].aoc.controls;
+                });
                 angular.forEach(controls, function (controlName) {
-                    control = new Control(new AocConfig(scope, controlName, element, $injector));
-                    $timeout(function () {
-                        console.log('timeout');
-                        Control.initControl(control);
-                    });
+                    control = new Control(new AocConfig(scope, controlName, element, function (configFn) {
+                        return $injector.invoke(configFn, this, {scope: scope, aocCompile: function (element) {
+                            $compile(element)(scope);
+                        }})
+                    }));
+                    (function (control) {
+                        $timeout(function () {
+                            Control.initControl(control);
+                        });
+                    }(control))
 
                 });
+
             }
         }
     });
@@ -237,7 +249,6 @@
             priority: 100,
             scope: true,
             link: function () {
-                console.log('scope');
             }
         }
     });
@@ -269,7 +280,6 @@
         return {
             priority: 999,
             link: function (scope, element, attr) {
-                console.log('options');
                 var evaluator = new Evaluator(scope),
                     prefixes = evaluator.eValue(attr.options),
                     optionsPrefix, optionPrefix, optionName, optionValue, control;
@@ -278,7 +288,7 @@
                 angular.forEach(prefixes, function (prefix, comp) {
                     optionsPrefix = new RegExp('^' + prefix + '[A-Z][A-Za-z]*$');
                     optionPrefix = new RegExp('^' + prefix);
-                    (element[0].aoc.controls) && (control = element[0].aoc.controls[prefix]);
+                    (element[0].aoc.controls) && (control = element[0].aoc.controls[comp]);
                     element[0].aoc.options[comp] = {};
                     angular.forEach(attr, function (expr, option) {
                         if (!optionsPrefix.test(option))return;
